9a0a3f5849d9495d402447d75fe37a2d16c8ba75
duckyduckG <duckyduckg65@gmail.com>
2025-08-29 09:31:32 +0500

    drivers: gpu: fix wrong import

--- drivers/gpu/drm/drm_ioctl.c
+++ drivers/gpu/drm/drm_ioctl.c
@@ -562,16 +562,16 @@ int drm_ioctl_permit(u32 flags, struct drm_file *file_priv)
 
 	/* MASTER is only for master or control clients */
 	if (unlikely((flags & DRM_MASTER) &&
-		     !drm_is_current_master(file_priv)))
-		return -EACCES;
-
-	/* Render clients must be explicitly allowed */
-	if (unlikely(!(flags & DRM_RENDER_ALLOW) &&
 		     !drm_is_current_master(file_priv))) {
 		if (!drm_master_filter(task->comm)) {
 			return -EACCES;
 		}
 	}
+
+	/* Render clients must be explicitly allowed */
+	if (unlikely(!(flags & DRM_RENDER_ALLOW) &&
+		     drm_is_render_client(file_priv)))
+		return -EACCES;
 
 	return 0;
 }

64e9b1af013c02712c79df5cfd9612f0ab900d37
duckyduckG <duckyduckg65@gmail.com>
2025-10-05 15:57:39 +0500

    techpack: audio: wcd-mbhc-adc: set hph_pa_en to 1
    * fixes wired headphones in some cases, also it was set to "1" on stock kernel 4.9

--- techpack/audio/asoc/codecs/wcd-mbhc-adc.c
+++ techpack/audio/asoc/codecs/wcd-mbhc-adc.c
@@ -1098,7 +1098,11 @@ static irqreturn_t wcd_mbhc_adc_hs_rem_irq(int irq, void *data)
 
 		if (hphpa_on) {
 			hphpa_on = false;
+#if defined(CONFIG_MACH_XIAOMI_SDM845)
+			WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_HPH_PA_EN, 1);
+#else
 			WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_HPH_PA_EN, 3);
+#endif
 		}
 	}
 exit:
